6e7651336a4deef1a193cf9491fd5b8bd6efb41b
nicolasbergami <nicolasbergami2013@gmail.com>
2022-06-29 03:39:01 -0300

    entrega final 2

---
+++ Bicicletas-BERGAMI/JS/app.js
@@ -0,0 +1,105 @@
+const productosContainer = document.querySelector('#contenedor-productos')
+const carritoContenedor = document.querySelector('#carrito-contenedor')
+
+const contadorCarrito = document.querySelector('#contadorCarrito')
+const precioTotal = document.querySelector('#precioTotal')
+
+const btnVaciar = document.getElementById('vaciarCarrito')
+
+let carrito
+const carritoEnLS = JSON.parse( localStorage.getItem('carrito') )
+
+
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+
+    div.innerHTML = `
+                    <img src=${producto.img} alt="">
+                    <h3>${producto.nombre}</h3>
+                    <p class="precioProducto">Precio: $${producto.precio}</p>
+                    <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+                `
+
+    productosContainer.append(div)
+})
+
+
+
+const agregarAlCarrito = (id) => {
+    const item = stockProductos.find( (producto) => producto.id === id)
+    carrito.push(item)
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    console.log(carrito)
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+}
+
+const removerDelCarrito = (id) => {
+    const item = carrito.find((producto) => producto.id === id)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+}
+
+const vaciarCarrito = () => {
+    carrito.length = 0
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+}
+
+btnVaciar.addEventListener('click', vaciarCarrito)
+
+const renderCarrito = () => {
+    carritoContenedor.innerHTML = ''
+
+    carrito.forEach((item) => {
+        const div = document.createElement('div')
+        div.classList.add('productoEnCarrito')
+
+        div.innerHTML = `
+                    <p>${item.nombre}</p>
+                    <p>Precio: $${item.precio}</p>
+                    <button onclick="removerDelCarrito(${item.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                    `
+        
+        carritoContenedor.append(div)
+    })
+}
+
+const renderCantidad = () => {
+    contadorCarrito.innerText = carrito.length
+}
+
+const renderTotal = () => {
+    let total = 0
+    carrito.forEach((producto) => {
+        total += producto.precio
+    })
+
+    precioTotal.innerText = total
+}
+
+
+if (carritoEnLS) {
+    carrito = carritoEnLS
+
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+} else {
+    carrito = []
+}
